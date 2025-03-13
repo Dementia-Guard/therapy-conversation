8de2d77daaaeaecde31fb42016aee7ed36ff7e3f
Vidusha T.Jayaweera <vidushatjayaweera@gmail.com>
2025-03-14 04:55:16 +0530

    changed deployment port

--- app.py
+++ app.py
@@ -289,5 +289,5 @@ def save_user(user_id):
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", 8000))
     app.run(host="0.0.0.0", port=port, debug=False)
